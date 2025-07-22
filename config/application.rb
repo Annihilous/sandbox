315f8e459f9eb174321c645a13a8d9ddc7db20c6
Annihilous <benjaminjnorton@gmail.com>
2025-07-22 09:35:17 -0700

    Update rails and rspec generators and gitignore.

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,15 @@ module Sandbox
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
 
+    # Generator configuration for Rails and RSpec
+    config.generators do |g|
+      g.helper false
+      g.helper_specs false
+      g.request_specs false
+      g.routing_specs false
+      g.view_specs false
+    end
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
